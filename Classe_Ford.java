39f3177b99dfac63ef0229b950e8677e4c8d6ab9
carlos121 <carlossousa470@gmail.com>
2017-05-09 11:20:20 -0300

    Add files via upload

---
+++ Classe_Ford.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ueg.programacao2.carro;
+
+/**
+ *
+ * @author Carlos Sousa
+ */
+public class Classe_Ford extends Classe_Carro{//A classe_Ford é uma sub classe da classe_Carro
+    
+}
